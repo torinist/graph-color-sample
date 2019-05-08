c56836caabcb0a579a78be7178914dd164f01b1d
大木美穂 <torino.torinist@gmail.com>
2019-05-09 00:06:42 +0900

    いくつかサンプル追加

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
-(function(){
-    function graphDraw() {
-        /* canvas要素のノードオブジェクト */
-        var canvas = document.getElementById('graphcolor');
-        /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
-        if ( ! canvas || ! canvas.getContext ) {
-          return false;
-        }
-        /* 2Dコンテキスト */
-        var ctx = canvas.getContext('2d');
-        /* 四角を描く */
-        ctx.beginPath();
-        ctx.fillStyle = 'rgba(128, 100, 162, 0.7)'; // 紫
-        ctx.moveTo(20, 140);    // スタート地点
-        ctx.lineTo(120, 60);   // 横終わり
-        ctx.lineTo(240, 240);   // 横終わり→縦終わり
-        ctx.lineTo(20, 240);    // 横始まり→縦終わり
-        ctx.closePath();
-        ctx.fill();
-    }
+function graphDraw(red, green, blue, alpha) {
+  /* canvas要素のノードオブジェクト */
+  var canvas = document.getElementById('graphcolor');
+  /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
+  if ( ! canvas || ! canvas.getContext ) {
+    return false;
+  }
+  /* 2Dコンテキスト */
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  /* 四角を描く */
+  ctx.beginPath();
+  ctx.fillStyle = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
+  ctx.moveTo(20, 140);    // スタート地点
+  ctx.lineTo(120, 60);   // 横終わり
+  ctx.lineTo(300, 300);   // 横終わり→縦終わり
+  ctx.lineTo(20, 300);    // 横始まり→縦終わり
+  ctx.closePath();
+  ctx.fill();
+}
 
-    function backDraw() {
-        /* canvas要素のノードオブジェクト */
-        var canvas = document.getElementById('backcolor');
-        /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
-        if ( ! canvas || ! canvas.getContext ) {
-          return false;
-        }
-        /* 2Dコンテキスト */
-        var ctx = canvas.getContext('2d');
-        /* 四角を描く */
-        ctx.beginPath();
-        ctx.fillStyle = 'rgba(192, 80, 77, 0.7)'; // 赤
-        ctx.moveTo(20, 20);     // スタート地点
-        ctx.lineTo(240, 20);    // 横終わり
-        ctx.lineTo(240, 240);   // 横終わり→縦終わり
-        ctx.lineTo(20, 240);    // 横始まり→縦終わり
-        ctx.closePath();
-        ctx.fill();
-    }
-    
+function backDraw(red, green, blue, alpha) {
+  /* canvas要素のノードオブジェクト */
+  var canvas = document.getElementById('backcolor');
+  /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
+  if ( ! canvas || ! canvas.getContext ) {
+    return false;
+  }
+  /* 2Dコンテキスト */
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  /* 四角を描く */
+  ctx.beginPath();
+  ctx.fillStyle = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
+  ctx.moveTo(20, 20);     // スタート地点
+  ctx.lineTo(300, 20);    // 横終わり
+  ctx.lineTo(300, 300);   // 横終わり→縦終わり
+  ctx.lineTo(20, 300);    // 横始まり→縦終わり
+  ctx.closePath();
+  ctx.fill();
+}
+
+function commentDraw(red, green, blue, alpha) {
+  let x = 200;   // 左上の頂点x座標
+  let y = 50;  // 左上の頂点y座標
+  let w = 90;   // 横の長さ
+  let h = 50;   // 縦の長さ
+  let r　= 10;  //角丸の半径
+
+  /* canvas要素のノードオブジェクト */
+  var canvas = document.getElementById('commentcolor');
+  /* canvas要素の存在チェックとCanvas未対応ブラウザの対処 */
+  if ( ! canvas || ! canvas.getContext ) {
+    return false;
+  }
+  /* 2Dコンテキスト */
+  var ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  /* 四角を描く */
+  ctx.beginPath();
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
+  ctx.fillStyle = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
+  ctx.moveTo(x,y + r);
+  ctx.arc(x+r,y+h-r,r,Math.PI,Math.PI*0.5,true);
+  ctx.arc(x+w-r,y+h-r,r,Math.PI*0.5,0,1);
+  ctx.arc(x+w-r,y+r,r,0,Math.PI*1.5,1);
+  ctx.arc(x+r,y+r,r,Math.PI*1.5,Math.PI,1);       
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+}
+
+(function(){
     window.onload = function() {
-        graphDraw();
-        backDraw();
+        graphDraw(255,255,255,1);
+        backDraw(0,0,0,1);
+        commentDraw(100,100,100,1);
     };
 })();
